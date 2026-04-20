4cd1a3e097ebc2333b686843d08c9b701582d9c7
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-04-20 08:17:44 +0545

    adding classs list

--- day15_javascsripts_intro/index.js
+++ day15_javascsripts_intro/index.js
@@ -1,2 +1,20 @@
 let a= document.getElementById('head');
-console.log(a);
+a.textContent="hmmm"
+console.log(a.textContent);
+
+
+const grid = document. querySelector(' [data-test-grid]');
+console. log(grid)
+
+// Create a new element
+const card = document. createElement ('div');
+card.classList.add('card');
+card.classList.add('new-card');
+card. textContent = 'New Card';
+
+// Append to the DOM
+document.querySelector('[data-test-grid]').appendChild(card);
+document.querySelector('[data-test-grid]').append(card,antherEL);
+document.querySelector('[data-test-grid]').prepend(card); // first child
+
+I

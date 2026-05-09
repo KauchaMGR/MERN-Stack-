e170f5aac41da5e470c75c88bb17e739fa530945
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-05-09 20:14:59 +0545

    adding neccesary comments

--- day15_javascsripts_intro/index.js
+++ day15_javascsripts_intro/index.js
@@ -1,6 +1,6 @@
-let a= document.getElementById('head');
-a.textContent="hmmm"
-console.log(a.textContent);
+// let a= document.getElementById('head');
+// a.textContent="hmmm"
+// console.log(a.textContent);
 
 
 const grid = document. querySelector(' [data-test-grid]');
@@ -12,9 +12,15 @@ card.classList.add('card');
 card.classList.add('new-card');
 card. textContent = 'New Card';
 
+const antherEL=document.createElement('div');
+antherEL.textContent='Another elements';
+
+
+
+
 // Append to the DOM
-document.querySelector('[data-test-grid]').appendChild(card);
-document.querySelector('[data-test-grid]').append(card,antherEL);
+// document.querySelector('[data-test-grid]').appendChild(card);
+ document.querySelector('[data-test-grid]').append(antherEL);
 document.querySelector('[data-test-grid]').prepend(card); // first child
 
 I

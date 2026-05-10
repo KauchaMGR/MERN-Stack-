cc77cd3972d1467883cce917258d79a4cb36b6ef
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-05-10 22:47:15 +0545

    adding the event listener at the add button

--- day15_javascsripts_intro/todo.js
+++ day15_javascsripts_intro/todo.js
@@ -2,3 +2,10 @@ const list=document.createElement('div');
 list.classList.add('list_item');
 list.textContent='walk 10k per day';
 document.querySelector('[data-test-contain]').append(list);
+
+
+const addbtn=document.querySelector('button[type="submit"]');
+addbtn.addEventListener('click',function(event){
+    event.preventDefault();
+    console.log('button was clicked');
+});

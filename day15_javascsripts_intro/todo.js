89521f3c6ebb16b3d8239e510c944e171b5d7459
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-05-11 07:18:33 +0545

    directly controlled by the form element

--- day15_javascsripts_intro/todo.js
+++ day15_javascsripts_intro/todo.js
@@ -4,8 +4,23 @@ list.textContent='walk 10k per day';
 document.querySelector('[data-test-contain]').append(list);
 
 
-const addbtn=document.querySelector('button[type="submit"]');
-addbtn.addEventListener('click',function(event){
-    event.preventDefault();
-    console.log('button was clicked');
+// const addbtn=document.querySelector('button[type="submit"]');
+// addbtn.addEventListener('click',function(event){
+//     event.preventDefault();
+//     console.log('button was clicked');
+// });
+
+const todoForm = document.querySelector('form');
+const todoInput = document.querySelector('input');
+todoForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents page reload
+
+    // const taskValue = todoInput.value.trim();
+
+    // if (taskValue !== "") {
+    //     addNewTodo(taskValue); // Adds to UI
+    //     saveToStorage(taskValue); // Adds to LocalStorage
+    //     todoInput.value = ''; // Clears the input field
+    // }
+    console.log('heyy i was ');
 });

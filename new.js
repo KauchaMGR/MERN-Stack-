a3724a11ae43acf97a2e6ee9e87ab306f754ad88
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-06-24 14:50:59 +0545

    adding the switch cases

--- new.js
+++ new.js
@@ -7,3 +7,45 @@ console.log(++x)
 if(x>7){
     console.log('hhehe');
 }
+
+
+let day = prompt("Enter num from 1 to 7 ");
+Number(day);
+
+switch (day) {
+case 1:
+console.log("Sunday")
+break;
+
+case 2:
+console.log("Monday")
+break;
+
+case 3:
+console. log("Tuesday")
+break;
+
+
+
+
+case 4:
+console.log("Wednesday")
+break;
+
+case 5:
+console.log("Thursday")
+break;
+
+
+case 6:
+console.log("Friday")
+break;
+
+case 7:
+console.log("Saturday")
+break;
+
+default:
+console.log("Invalid input")
+break;
+}

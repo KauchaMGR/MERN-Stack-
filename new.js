b3f974a8b3499721b903a47c0d01a4829a35d39c
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-06-25 13:54:17 +0545

    addding for

--- new.js
+++ new.js
@@ -1,51 +1,92 @@
-console.log('hehe');
+// console.log('hehe');
 
 
-let x=7;
-console.log(x++)
-console.log(++x)
-if(x>7){
-    console.log('hhehe');
-}
+// let x=7;
+// console.log(x++)
+// console.log(++x)
+// if(x>7){
+//     console.log('hhehe');
+// }
 
 
-let day = prompt("Enter num from 1 to 7 ");
-Number(day);
+// let day = prompt("Enter num from 1 to 7 ");
+// Number(day);
 
-switch (day) {
-case 1:
-console.log("Sunday")
-break;
+// switch (day) {
+// case 1:
+// console.log("Sunday")
+// break;
 
-case 2:
-console.log("Monday")
-break;
+// case 2:
+// console.log("Monday")
+// break;
 
-case 3:
-console. log("Tuesday")
-break;
-
-
+// case 3:
+// console. log("Tuesday")
+// break;
 
 
-case 4:
-console.log("Wednesday")
-break;
-
-case 5:
-console.log("Thursday")
-break;
 
 
-case 6:
-console.log("Friday")
-break;
+// case 4:
+// console.log("Wednesday")
+// break;
 
-case 7:
-console.log("Saturday")
-break;
+// case 5:
+// console.log("Thursday")
+// break;
 
-default:
-console.log("Invalid input")
-break;
-}
+
+// case 6:
+// console.log("Friday")
+// break;
+
+// case 7:
+// console.log("Saturday")
+// break;
+
+// default:
+// console.log("Invalid input")
+// break;
+// }
+
+
+// const Cards = ["A", "2", "3", "4", "5","6","7","8","9", "10", "J","Q", "K"]
+
+// const randomNum = Math.floor((Math.random()* Cards. length -1) + 1);
+
+// console.log(Cards[randomNum]);
+
+// const users = [{
+// id: 1,
+// name: "Akil",
+// age: 25,
+// isStudent: false
+// }
+// ,
+// {id: 2,
+// name: "Arun",
+// age: 20,
+// isStudent: true
+// }
+// ,
+// {id: 3,
+// name: "Karun",
+// age: 21,
+// isStudent: false}
+// ,
+// {id: 4,
+// name: "Aman",
+// age: 22,
+// isStudent: true}
+// ,
+// {id: 5,
+// name: "Raman",
+// age: 23,
+// isStudent: false}
+// ];
+
+// let BigUser = users.filter((user) => user.age > 22);
+// console.log(BigUser);
+
+console.log('helloo kaucha ');

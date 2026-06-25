4e56d5306d5d9c778509b8f8b0ba3a37ac6eb1cd
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-06-25 15:18:20 +0545

    Event delegation

--- new_bacth/day2.js
+++ new_bacth/day2.js
@@ -1,19 +1,30 @@
-function eat() {
-    console.log("what to eat");
-}
+// function eat() {
+//     console.log("what to eat");
+// }
 
-function go(callback) {
-    console.log("where to go?");
-    callback(); // at first this gets executed
-}
+// function go(callback) {
+//     console.log("where to go?");
+//     callback(); // at first this gets executed
+// }
 
-go(eat);
+// go(eat);
 
-const btn = document.getElementById('btn');
+// const btn = document.getElementById('btn');
 
-btn.addEventListener('click', (e) => {
-    e.target.textContent = "button was clicked";
-});
+// btn.addEventListener('click', (e) => {
+//     e.target.textContent = "button was clicked";
+// });
 
-let input = document.getElementById('inp');
-input.value = "hello k xa";
+// let input = document.getElementById('inp');
+// input.value = "hello k xa";
+
+// //event bubbling
+// let div=document.querySelector('.button');
+
+
+//Event delegations example
+let li_item =document.getElementById("lists");//direct apply on the parent
+li_item.addEventListener('click',(e)=>{
+    e.stopPropagation();
+    console.log(e.target.textContent);
+})

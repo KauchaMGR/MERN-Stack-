a8b5ef9efd24711f806ebd178f094d895a758e7f
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-06-25 14:31:52 +0545

    adding event listener

--- new_bacth/day2.js
+++ new_bacth/day2.js
@@ -1,13 +1,19 @@
-//call back function
-
-function eat(){
+function eat() {
     console.log("what to eat");
 }
 
-function go(callback){
+function go(callback) {
     console.log("where to go?");
-    callback();// at first this get executed
-
+    callback(); // at first this gets executed
 }
 
 go(eat);
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', (e) => {
+    e.target.textContent = "button was clicked";
+});
+
+let input = document.getElementById('inp');
+input.value = "hello k xa";

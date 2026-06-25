972e90dee5e3b7d2bfebd69270082f2b3957211c
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-06-25 14:08:36 +0545

    call back function

--- new_bacth/day2.js
+++ new_bacth/day2.js
@@ -1,1 +1,13 @@
-console.log('hello');
+//call back function
+
+function eat(){
+    console.log("what to eat");
+}
+
+function go(callback){
+    console.log("where to go?");
+    callback();// at first this get executed
+
+}
+
+go(eat);

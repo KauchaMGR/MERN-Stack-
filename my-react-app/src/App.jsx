f0c003b89f1ba06d38136ba75704c3f4aab53f10
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-08-31 10:32:48 +0545

    adding the background color

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Components/layouts/Header'
 import Footer from './Components/layouts/Footer'
 import Card from './Components/Card'
@@ -7,24 +7,32 @@ import Component from './Components/Component'
 import Count from './Components/Count'
 
 function App() {
+  const [bgColor, setBgColor] = useState('white')
 
   const handleClick = () => {
     alert('  save Button  was clicked!')
   }
+
   const deleteClick = () => {
     alert('  delete Button  was clicked!')
   }
 
+  const changeBg = () => {
+   setBgColor((prev)=>(prev==='white'?'black':'white'))
+  }
+
   return (
-    <div className="min-h-screen w-full max-w-6xl mx-auto bg-slate-100 px-4 py-6 text-slate-800">
+    <div
+      className="min-h-screen w-full max-w-6xl mx-auto px-4 py-6 text-slate-800"
+      style={{ backgroundColor: bgColor }}
+    >
       <Header />
       <Card name="John Doe" />
       <Card name="Jane Smith" />
       <Button label="Save" onClick={handleClick} color="blue" />
       <Button label="Delete" onClick={deleteClick} color="red" />
       <Component />
-
-      <Count />
+      <Count mode={changeBg} color={bgColor} />
       <Footer />
     </div>
   )
--- my-react-app/src/Components/Count.jsx
+++ my-react-app/src/Components/Count.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
-function Count() {
-    const [count, setCount] =useState(0);
-   const increment =() =>{
-    setCount(count + 1);
-    console.log(count);
-   }
-  return (
-    <div>
-        <div>{count}</div>
-        <button onClick={increment} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer mb-2'>Increment</button>
-    </div>
-  )
+function Count({ mode, color }) {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount((prevCount) => prevCount + 1)
+    }
+
+    return (
+        <div className="p-4" style={{ backgroundColor: color }}>
+            <div>{count}</div>
+            <button
+                onClick={increment}
+                className="mb-2 cursor-pointer rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            >
+                Increment
+            </button>
+            <button
+                onClick={mode}
+                className="mb-2 cursor-pointer rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            >
+                Mode
+            </button>
+        </div>
+    )
 }
 
 export default Count

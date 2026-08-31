c84a595a2e81e81be3438b88aba0600c02a8eb84
Dipeen Kaucha Magar <kauchadipeen@gmail.com>
2026-08-31 14:09:38 +0545

    adding the rol the dice function

--- my-react-app/src/Components/layouts/Footer.jsx
+++ my-react-app/src/Components/layouts/Footer.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React,{useState}from 'react'
 
 const Footer = () => {
+  const [dice, setDice] = useState(0)
+
+  const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    setDice(randomNumber)
+  }
   return (
     <>
 
     <div>
      < h1 className="text-3xl  text-center font-bold text-red-500">This is footer</h1>
     </div>
+     <button onClick={rollDice} className="mb-2 cursor-pointer rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+        Roll Dice
+      </button>
+      <div className="text-center text-2xl font-bold mt-4">
+        {dice === 1 && '🎲'}
+        {dice === 2 && '🎲🎲'}
+        {dice === 3 && '🎲🎲🎲'}
+        {dice === 4 && '🎲🎲🎲🎲'}
+        {dice === 5 && '🎲🎲🎲🎲🎲'}
+        {dice === 6 && '🎲🎲🎲🎲🎲🎲'}
+      </div>
+
     </>
   )
 }
